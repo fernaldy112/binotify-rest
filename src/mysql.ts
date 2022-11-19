f8974a9bfebda18477612e92223df0d56faf043d
Radenz <13520166@std.stei.itb.ac.id>
2022-11-20 00:11:46 +0700

    refactor: refactor query calls

--- src/mysql.ts
+++ src/mysql.ts
@@ -8,3 +8,23 @@ export const pool = mysql.createPool({
   database: ENV.DB_NAME,
   port: +String(ENV.DB_PORT),
 });
+
+export function query(query: string) {
+  return new Promise<any[]>((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        reject(err);
+      }
+
+      connection.query(query, (err, result, _) => {
+        connection.release();
+
+        if (err) {
+          reject(err);
+        }
+
+        resolve(result);
+      });
+    });
+  });
+}
