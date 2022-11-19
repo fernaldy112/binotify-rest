16aaa0e1ad8ef8d3d2a026b60434369fce3ee182
Radenz <13520166@std.stei.itb.ac.id>
2022-11-20 00:12:26 +0700

    feat: add song creation endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,60 @@
 import express from "express";
 import cors from "cors";
 import { ENV } from "./environment";
-import { getSongById } from "./song";
+import { getSongById, getSongCount, insertSong } from "./song";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 // TODO: add endpoints
+
+// Create
+app.post("/song", async (req, res) => {
+  const { judul, penyanyi_id, audio_path } = req.body;
+
+  if (!judul || !penyanyi_id || !audio_path) {
+    res.status(400);
+    res.end();
+    return;
+  }
+
+  await insertSong({
+    judul,
+    penyanyi_id,
+    audio_path,
+  });
+
+  res.end();
+});
+
+// Read
 app.get("/song/:id", async (req, res) => {
   const songId = req.params.id;
+
+  if (!songId) {
+    res.status(400);
+    res.end();
+    return;
+  }
 
   res.json(await getSongById(+songId));
 
   res.end();
 });
 
-app.get("/test", async (req, res) => {
-  res.write("Hello World!");
+// Update
+app.get("/song/:id", async (req, res) => {
+  const songId = req.params.id;
+
+  if (!songId) {
+    res.status(400);
+    res.end();
+    return;
+  }
+
+  res.json(await getSongById(+songId));
 
   res.end();
 });
