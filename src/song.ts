f1398080493d992c991937fb91753575fb62abdb
Radenz <13520166@std.stei.itb.ac.id>
2022-11-20 00:12:15 +0700

    feat: add song crud functions

--- src/song.ts
+++ src/song.ts
@@ -1,26 +1,48 @@
-import { pool } from "./mysql";
+import { Song, SongDetails } from "./model/song";
+import { query } from "./mysql";
 
 async function getSongById(id: number) {
-  return new Promise((resolve, reject) => {
-    pool.getConnection((err, connection) => {
-      if (err) {
-        reject(err);
-      }
+  return query(`SELECT * FROM song WHERE song_id = ${id};`);
+}
 
-      connection.query(
-        `SELECT * FROM song WHERE song_id = ${id};`,
-        (err, result, fields) => {
-          connection.release();
+async function insertSong(song: SongDetails) {
+  const { judul, penyanyi_id, audio_path } = song;
+  return query(`INSERT INTO song VALUES (
+    NULL,
+    '${judul}',
+    ${penyanyi_id},
+    '${audio_path}'
+  );`);
+}
 
-          if (err) {
-            reject(err);
-          }
+async function updateSong(song: Song) {
+  const { song_id, judul, penyanyi_id, audio_path } = song;
+  let UPDATES = [];
+  if (judul !== undefined) {
+    UPDATES.push(`judul = ${judul}`);
+  }
+  if (penyanyi_id !== undefined) {
+    UPDATES.push(`penyanyi_id = ${penyanyi_id}`);
+  }
+  if (audio_path !== undefined) {
+    UPDATES.push(`audio_path = ${audio_path}`);
+  }
 
-          resolve(result);
-        }
-      );
-    });
+  return query(
+    `UPDATE song SET ${UPDATES.join(", ")} WHERE song_id = ${song_id}`
+  );
+}
+
+async function deleteSongById(id: number) {
+  return query(`DELETE FROM song WHERE song_id = ${id};`);
+}
+
+async function getSongCount() {
+  return query(`SELECT COUNT(*) AS count FROM song;`).then((rawData) => {
+    console.log(rawData);
+    console.log(rawData[0]["count"]);
+    return rawData[0]["count"];
   });
 }
 
-export { getSongById };
+export { getSongById, insertSong, updateSong, deleteSongById, getSongCount };
